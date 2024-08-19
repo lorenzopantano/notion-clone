2d5665fff235eb5ab4290d05bb42f0ffa53a2468
Lorenzo Pantano <l.pantano97@gmail.com>
2024-08-19 12:47:57 +0200

    feat: trash box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,11 +2,13 @@
 
 import DocumentList from "@/app/(main)/_components/document-list";
 import Item from "@/app/(main)/_components/item";
+import TrashBox from "@/app/(main)/_components/trash-box";
 import UserItem from "@/app/(main)/_components/user-item";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
 import { useMutation, useQuery } from "convex/react";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useRef, useState } from "react";
 import { toast } from "sonner";
@@ -130,6 +132,15 @@ export default function Navigation() {
 				</div>
 				<div className="mt-4">
 					<DocumentList />
+					<Item onClick={handleCreate} icon={Plus} label="Add a page" />
+					<Popover>
+						<PopoverTrigger className="w-full mt-4">
+							<Item label="Trash" icon={Trash} />
+						</PopoverTrigger>
+						<PopoverContent side={isMobile ? "bottom" : "right"} className="p-0 w-72">
+							<TrashBox />
+						</PopoverContent>
+					</Popover>
 				</div>
 				<div
 					onMouseDown={handleMouseDown}
---
+++ app/(main)/_components/trash-box.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/confirm-modal";
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+import { Search, Trash, Undo } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "sonner";
+
+export default function TrashBox() {
+	const router = useRouter();
+	const params = useParams();
+	const documents = useQuery(api.documents.getTrash);
+	const restore = useMutation(api.documents.restore);
+	const remove = useMutation(api.documents.remove);
+
+	const [search, setSearch] = React.useState("");
+
+	const filteredDocuments = documents?.filter((document) =>
+		document.title.toLowerCase().includes(search.toLowerCase())
+	);
+	console.log(filteredDocuments);
+
+	const onClick = (documentId: string) => {
+		router.push(`/documents/${documentId}`);
+	};
+
+	function onRestore(
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+		documentId: Id<"documents">
+	) {
+		event.stopPropagation();
+		const promise = restore({ id: documentId });
+		toast.promise(promise, {
+			loading: "Restoring...",
+			success: "Document restored",
+			error: "Failed to restore document",
+		});
+	}
+
+	function onRemove(documentId: Id<"documents">) {
+		const promise = remove({ id: documentId });
+		toast.promise(promise, {
+			loading: "Removing...",
+			success: "Document removed",
+			error: "Failed to remove document",
+		});
+	}
+
+	if (documents === undefined) {
+		return (
+			<div className="h-full flex items-center justify-center p-4">
+				<Spinner size="lg" />
+			</div>
+		);
+	}
+
+	return (
+		<div className="text-sm">
+			<div className="flex items-center gap-x-1 p-2">
+				<Search className="h-4 w-4" />
+				<Input
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+					className="h-7 px-2 focuring-transparent bg-secondary"
+					placeholder="Filter by page name..."
+				/>
+			</div>
+			<div className="mt-2 px-1 pb-1">
+				<p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+					No document found.
+				</p>
+				{filteredDocuments?.map((document) => {
+					return (
+						<div
+							role="button"
+							key={document._id}
+							onClick={() => onClick(document._id)}
+							className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+						>
+							<span className="truncate pl-2">{document.title}</span>
+							<div className="flex items-center">
+								<div
+									role="button"
+									onClick={(e) => onRestore(e, document._id)}
+									className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+								>
+									<Undo className="w-4 h-4 text-muted-foreground" />
+								</div>
+								<ConfirmModal onConfirm={() => onRemove(document._id)}>
+									<div className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600">
+										<Trash className="w-4 h-4 text-muted-foreground" />
+									</div>
+								</ConfirmModal>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
